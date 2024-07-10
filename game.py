e022403d5b6c0ec67fdd3fbfb4367a64619034da
donghee.lim <ldh5730@naver.com>
2024-07-10 14:39:38 +0900

    [refactor] 4 - assert 조건 캡슐화

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 class Game:
     def guess(self, number):
+        self.assert_illegal_value(number)
+
+    def assert_illegal_value(self, number):
         if number is None:
             raise TypeError("None은 사용 할 수 없습니다.")
         if len(number) != 3:
             raise TypeError("자릿수가 3자리 이어야 합니다.")
-
         if not all(ord("0") <= ord(char) <= ord("9") for char in number):
             raise TypeError()
-
         if self.is_duplicated(number):
             raise TypeError()
 
