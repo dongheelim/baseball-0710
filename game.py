04ce85012802e7023fdc6f81bdd3fdad109abf97
donghee.lim <ldh5730@naver.com>
2024-07-10 15:07:40 +0900

    [refactor] 8

--- game.py
+++ game.py
@@ -9,18 +9,24 @@ class Game:
     def guess(self, number):
         self.assert_illegal_value(number)
 
-        if number == self.question:
-            return GameResult(True, 3, 0)
+        if self.is_solved(number):
+            return self.get_success_game_result()
         else:
             strikes = 0
             balls = 0
             for idx, q_number in enumerate(self.question):
-                strikes += int(number[idx]==q_number)
+                strikes += int(number[idx] == q_number)
 
             for idx, q_number in enumerate(self.question):
-                balls += int(number[idx]!=q_number and q_number in number)
+                balls += int(number[idx] != q_number and q_number in number)
 
-            return GameResult(False, strikes  , balls)
+            return GameResult(False, strikes, balls)
+
+    def get_success_game_result(self):
+        return GameResult(True, 3, 0)
+
+    def is_solved(self, number):
+        return number == self.question
 
     def assert_illegal_value(self, number):
         if number is None:
